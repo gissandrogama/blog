dadc1d7a77754ceafecbba6f3a28514cdd4d52e7
gissandrogama <gissandrogama@gmail.com>
2021-04-27 23:51:12 -0300

    feat(controller): add functionalities in index chore(layout): add link feat(layout): add post and index feat(view): add post view

--- lib/blog_web/controllers/post_controller.ex
+++ lib/blog_web/controllers/post_controller.ex
@@ -1,7 +1,8 @@
 defmodule BlogWeb.PostController do
   use BlogWeb, :controller
 
-  def index(conn, params) do
-    
+  def index(conn, _params) do
+    posts = [%{title: "Phoenix"}, %{title: "LiveView"}, %{title: "Elixir"}]
+    render(conn, "index.html", posts: posts)
   end
 end

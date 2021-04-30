fdb7e06f1c5b3bbfa71b0e2f35366fc50acf0df8
gissandrogama <gissandrogama@gmail.com>
2021-04-30 00:52:34 -0300

    feat(blog/post): add schema feat(controller/post): add function show

--- lib/blog_web/controllers/post_controller.ex
+++ lib/blog_web/controllers/post_controller.ex
@@ -2,7 +2,11 @@ defmodule BlogWeb.PostController do
   use BlogWeb, :controller
 
   def index(conn, _params) do
-    posts = [%{title: "Phoenix"}, %{title: "LiveView"}, %{title: "Elixir"}]
+    posts = [%{id: 1, title: "Phoenix"}, %{id: 2, title: "LiveView"}, %{id: 3, title: "Elixir"}]
     render(conn, "index.html", posts: posts)
+  end
+
+  def show(conn, params) do
+    render(conn, "show.html")
   end
 end

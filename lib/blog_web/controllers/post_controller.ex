46f67adfa619b52894e96a5b65ed60ead47fbf0e
GISSANDRO M DANTAS GAMA <gissandrogama@gmail.com>
2021-05-02 01:00:09 -0300

    Feature/start materialize (#14)
    * feate(config): add materialize
    
    * feate(template): add navbar
    
    * feat(router): create router list posts
    
    * feat(controller): add functionalities in index
    chore(layout): add link
    feat(layout): add post and index
    feat(view): add post view
    
    * feat(assets/app): add function
    
    * feat(blog/post): add schema
    feat(controller/post): add function show
    
    * feat(blog_web): add post and migrate
    
    * fix(doc): add mofule doc
    
    * feat(controller): integration with db
    
    * feat(template/post): returne data post
    
    * fix(migrate): change type field
    feat(seeds): add data
    
    * chore(style): formating code

---
+++ lib/blog_web/controllers/post_controller.ex
@@ -0,0 +1,15 @@
+defmodule BlogWeb.PostController do
+  use BlogWeb, :controller
+
+  alias Blog.Posts.Post
+
+  def index(conn, _params) do
+    posts = Post |> Blog.Repo.all()
+    render(conn, "index.html", posts: posts)
+  end
+
+  def show(conn, %{"id" => id}) do
+    post = Blog.Repo.get!(Post, id)
+    render(conn, "show.html", post: post)
+  end
+end
--- lib/blog_web/router.ex
+++ lib/blog_web/router.ex
@@ -16,6 +16,8 @@ defmodule BlogWeb.Router do
   scope "/", BlogWeb do
     pipe_through :browser
 
+    get "posts", PostController, :index
+    get "posts/:id", PostController, :show
     get "/", PageController, :index
   end
 

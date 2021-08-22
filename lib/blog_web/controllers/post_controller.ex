7112bfbfaec44e14eec375fa3c5efa08d0b988be
GISSANDRO M DANTAS GAMA <gissandrogama@gmail.com>
2021-08-22 00:42:06 -0300

    Feature: start materialize (#16)
    * feate(config): add materialize
    
    * feate(template): add navbar
    
    * feat(router): create router list posts
    
    * feat(controller): add functionalities in index
    chore(layout): add link
    feat(layout): add post and index
    feat(view): add post view
    
    * feat(assets/app): add function
    
    * feat(blog/post): add schema
    feat(controller/post): add function show
    
    * feat(blog_web): add post and migrate
    
    * fix(doc): add mofule doc
    
    * feat(controller): integration with db
    
    * feat(template/post): returne data post
    
    * fix(migrate): change type field
    feat(seeds): add data
    
    * chore(style): formating code
    
    * feat: creat form new post
    
    * feat: creat posts
    
    * feat: update to context
    
    * feat: test context of posts
    
    * feat: tests post controller
    
    Co-authored-by: gissandromdgama <gissandro@contaswap.com>

--- lib/blog_web/controllers/post_controller.ex
+++ lib/blog_web/controllers/post_controller.ex
@@ -1,19 +1,60 @@
 defmodule BlogWeb.PostController do
   use BlogWeb, :controller
 
-  alias Blog.Posts.Post
+  alias Blog.{Posts, Posts.Post}
 
   def index(conn, _params) do
-    posts = Post |> Blog.Repo.all()
+    posts = Posts.list_posts()
     render(conn, "index.html", posts: posts)
   end
 
   def show(conn, %{"id" => id}) do
-    post = Blog.Repo.get!(Post, id)
+    post = Posts.get_post!(id)
     render(conn, "show.html", post: post)
   end
 
   def new(conn, _params) do
-    render(conn, "new.html")
+    changeset = Post.changeset(%Post{})
+    render(conn, "new.html", changeset: changeset)
+  end
+
+  def edit(conn, %{"id" => id}) do
+    post = Posts.get_post!(id)
+    changeset = Post.changeset(post)
+    render(conn, "edit.html", post: post, changeset: changeset)
+  end
+
+  def create(conn, %{"post" => post}) do
+    case Posts.create_post(post) do
+      {:ok, post} ->
+        conn
+        |> put_flash(:info, "Post criado com sucesoo!")
+        |> redirect(to: Routes.post_path(conn, :show, post))
+
+      {:error, changeset} ->
+        render(conn, "new.html", changeset: changeset)
+    end
+  end
+
+  def update(conn, %{"id" => id, "post" => post_params}) do
+    post = Posts.get_post!(id)
+
+    case Posts.update_post(post, post_params) do
+      {:ok, post} ->
+        conn
+        |> put_flash(:info, "Post atualizado com sucesoo!")
+        |> redirect(to: Routes.post_path(conn, :show, post))
+
+      {:error, changeset} ->
+        render(conn, "edit.html", changeset: changeset, post: post)
+    end
+  end
+
+  def delete(conn, %{"id" => id}) do
+    Posts.delete(id)
+
+    conn
+    |> put_flash(:info, "Post foi deletado")
+    |> redirect(to: Routes.post_path(conn, :index))
   end
 end

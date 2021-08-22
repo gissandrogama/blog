4e05822558c86d5eb60843f016a0b1e0bb6bd2b6
gissandromdgama <gissandro@contaswap.com>
2021-08-22 00:30:18 -0300

    feat: tests post controller

--- test/blog_web/controllers/post_controller_test.exs
+++ test/blog_web/controllers/post_controller_test.exs
@@ -4,13 +4,88 @@ defmodule BlogWeb.PostControllerTest do
   """
   use BlogWeb.ConnCase
 
-  test "GET all posts /", %{conn: conn} do
-    conn = get(conn, "/posts")
-    assert html_response(conn, 200) =~ "All posts"
+  @valid_post %{
+    title: "Phoenix framework",
+    description: "Lorem"
+  }
+
+  @update_post %{
+    title: "Update Phoenix framework",
+    description: "Update Lorem"
+  }
+
+  def fixture(:post) do
+    {:ok, post} = Blog.Posts.create_post(@valid_post)
+    post
   end
 
-  # test "GET show posts /", %{conn: conn} do
-  #   conn = get(conn, "/posts/1")
-  #   assert html_response(conn, 200) =~ "Phoenix framework"
-  # end
+  test "GET all posts /", %{conn: conn} do
+    Blog.Posts.create_post(@valid_post)
+    conn = get(conn, Routes.post_path(conn, :index))
+    assert html_response(conn, 200) =~ "Phoenix framework"
+  end
+
+  test "GET one post for id", %{conn: conn} do
+    {:ok, post} = Blog.Posts.create_post(@valid_post)
+    conn = get(conn, Routes.post_path(conn, :show, post))
+    assert html_response(conn, 200) =~ "Phoenix framework"
+  end
+
+  test "enter the post creation form", %{conn: conn} do
+    conn = get(conn, Routes.post_path(conn, :new))
+    assert html_response(conn, 200) =~ "Criar Post"
+  end
+
+  test "enter the post edit form", %{conn: conn} do
+    {:ok, post} = Blog.Posts.create_post(@valid_post)
+    conn = get(conn, Routes.post_path(conn, :edit, post))
+    assert html_response(conn, 200) =~ "Editar Post"
+  end
+
+  test "create post", %{conn: conn} do
+    conn = post(conn, Routes.post_path(conn, :create), post: @valid_post)
+
+    assert %{id: id} = redirected_params(conn)
+    assert redirected_to(conn) == Routes.post_path(conn, :show, id)
+
+    conn = get(conn, Routes.post_path(conn, :show, id))
+    assert html_response(conn, 200) =~ "Phoenix framework"
+  end
+
+  test "create post with params ivalid", %{conn: conn} do
+    conn = post(conn, Routes.post_path(conn, :create), post: %{})
+    assert html_response(conn, 200) =~ "campo obrigatório!"
+  end
+
+  describe "The tests below depend on a post created" do
+    setup [:create_post]
+
+    test "updated post", %{conn: conn, post: post} do
+      conn = put(conn, Routes.post_path(conn, :update, post), post: @update_post)
+
+      assert %{id: id} = redirected_params(conn)
+      assert redirected_to(conn) == Routes.post_path(conn, :show, id)
+
+      conn = get(conn, Routes.post_path(conn, :show, id))
+      assert html_response(conn, 200) =~ "Update Phoenix framework"
+    end
+
+    test "update post with params ivalid", %{conn: conn, post: post} do
+      conn =
+        put(conn, Routes.post_path(conn, :update, post), post: %{title: nil, description: nil})
+
+      assert html_response(conn, 200) =~ "Editar Post"
+    end
+
+    test "delete post", %{conn: conn, post: post} do
+      conn = delete(conn, Routes.post_path(conn, :delete, post))
+      assert redirected_to(conn) == Routes.post_path(conn, :index)
+
+      assert_error_sent 404, fn -> get(conn, Routes.post_path(conn, :show, post)) end
+    end
+  end
+
+  defp create_post(_) do
+    %{post: fixture(:post)}
+  end
 end

0b3aed2963d4de6a5d8a21cfb262b48e7cfee1d6
gissandromdgama <gissandro@contaswap.com>
2021-09-06 20:52:10 -0300

    chore: adding comments to the socket

--- lib/blog_web/channels/comments_channel.ex
+++ lib/blog_web/channels/comments_channel.ex
@@ -1,7 +1,12 @@
 defmodule BlogWeb.CommentsChannel do
   use BlogWeb, :channel
 
-  def join(name, _payload, socket), do: {:ok, %{name: "Deu certo!!!"}, socket}
+  def join("comments:" <> post_id, _payload, socket) do
+    post = Blog.Posts.get_post_with_comments!(post_id)
+
+    {:ok, %{comments: post.comments}, socket}
+  end
+
 
   def handle_in() do
   end

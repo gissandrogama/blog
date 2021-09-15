4e364192ffd779d6b2cb8fcc8ea801845a01a65b
gissandrogama <gissandrogama@gmail.com>
2021-09-14 23:29:14 -0300

    feat: add button menu login and logout

--- lib/blog_web/controllers/plugs/set_user.ex
+++ lib/blog_web/controllers/plugs/set_user.ex
@@ -15,8 +15,8 @@ defmodule BlogWeb.Plugs.SetUser do
       user = user_id && Accounts.get_user!(user_id) ->
         assign(conn, :user, user)
 
-        true ->
-          assign(conn, :user, nil)
+      true ->
+        assign(conn, :user, nil)
     end
   end
 end
